50544361c20693e6466f9bc1962adb2767375d96
robin.kim@coxinc.com <88211687+robinhwhwk@users.noreply.github.com>
2024-09-14 16:58:51 -0400

    Use hash router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+import './styles/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
